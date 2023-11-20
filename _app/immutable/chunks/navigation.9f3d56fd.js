9b46b059747055fb3f5fa76eb8aceebf6d92e299
DongYounYim <dyyim4725@gmail.com>
2023-11-20 17:16:08 +0900

    Updates

--- _app/immutable/chunks/navigation.9f3d56fd.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./singletons.c6ea3df6.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
---
+++ _app/immutable/chunks/navigation.a47ca5d6.js
@@ -0,0 +1,1 @@
+import{k as o}from"./singletons.0591c93f.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
