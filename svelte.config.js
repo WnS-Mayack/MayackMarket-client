cd3ae93fcd312293251a3a3fa191350ce688817c
DongyounYim <dyyim4725@gmail.com>
2023-11-20 21:32:05 +0900

    [deploy] version 1.0.2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-import replace from '@rollup/plugin-replace';
-import dotenv from 'dotenv';
+// import replace from '@rollup/plugin-replace';
+// import dotenv from 'dotenv';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const env = dotenv.config().parsed;
+// const env = dotenv.config().parsed;
 
 const dev = process.argv.includes('dev');
 
@@ -11,16 +11,7 @@ const dev = process.argv.includes('dev');
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		replace({
-			preventAssignment: true,
-			values: {
-				'process.env.VUE_APP_API_BASE_URL': JSON.stringify(env.VUE_APP_API_BASE_URL),
-				'process.env.VUE_APP_IMAGE_BASE_URL': JSON.stringify(env.VUE_APP_IMAGE_BASE_URL)
-			}
-		})
-	],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
